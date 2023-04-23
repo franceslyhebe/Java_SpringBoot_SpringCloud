32126fd4794cad4ea56476322247be5e5c16840a
Francesly Hebe <f.francesly@gmail.com>
2023-04-23 18:05:32 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/workers")
+@RefreshScope
 public class WorkerResource {
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
     @Autowired
