6e4cd425b56707ed43cd611001830f6643d56b6d
Francesly Hebe <f.francesly@gmail.com>
2023-04-30 21:02:09 -0300

    Login e Token JWT

--- hr-oauth/src/main/java/com/devsuperior/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/service/UserService.java
@@ -3,14 +3,26 @@ package com.devsuperior.hroauth.service;
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserFeignClient userFeignClient;
-    public User findByEmail(String email){
+
+    public User findByEmail(String email) {
         User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null) {
+            throw new IllegalArgumentException("Email not found");
+        }
+        return user;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
         if(user == null){
             throw new IllegalArgumentException("Email not found");
         }
